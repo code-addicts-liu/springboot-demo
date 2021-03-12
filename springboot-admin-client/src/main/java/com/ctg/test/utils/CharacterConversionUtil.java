a1b55df028e5bbda12f77238e64589c264b023a5
liuyue <liuyue@tydic.com>
2021-03-12 10:24:00 +0800

    工具类

---
+++ springboot-admin-client/src/main/java/com/ctg/test/utils/CharacterConversionUtil.java
@@ -0,0 +1,165 @@
+package com.ctg.test.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class CharacterConversionUtil {
+    /** 转换为驼峰（大写）
+     * 
+     * @param underscoreName
+     * @return */
+    public static String camelCaseName(String underscoreName) {
+        StringBuilder result = new StringBuilder();
+        if (underscoreName != null && underscoreName.length() > 0) {
+            boolean flag = false;
+            for (int i = 0; i < underscoreName.length(); i++) {
+                char ch = underscoreName.charAt(i);
+                if ("_".charAt(0) == ch) {
+                    flag = true;
+                }
+                else {
+                    if (flag) {
+                        result.append(Character.toUpperCase(ch));
+                        flag = false;
+                    }
+                    else {
+                        result.append(Character.toLowerCase(ch));
+                    }
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    /** 转换为下划线(大写)
+     * 
+     * @param camelCaseName
+     * @return */
+    public static String underscoreName(String camelCaseName) {
+        StringBuilder result = new StringBuilder();
+        int len = camelCaseName.length();
+        if (camelCaseName != null && len > 0) {
+            result.append(camelCaseName.substring(0, 1).toUpperCase());
+            for (int i = 1; i < len; i++) {
+                char ch = camelCaseName.charAt(i);
+                if (Character.isUpperCase(ch)) {
+                    result.append("_");
+                    result.append(ch);
+                }
+                else {
+                    result.append(Character.toUpperCase(ch));
+                }
+            }
+        }
+        return result.toString();
+    }
+    
+    /**
+     * 适用场景: 转换String类型的json串，的 key为下划线大写
+     * 调用方式: 
+     * 业务逻辑说明
+    */
+    public final static Object convertToUpperUnderline(String json) {
+        Object obj = JSON.parse(json);
+        convertToUpperUnderline(obj);
+        return obj;
+    }
+
+    /**
+     * 适用场景: 递归转换json key为下划线大写
+     * 调用方式: 
+     * 业务逻辑说明
+    */
+    private final static void convertToUpperUnderline(Object json) {
+        if (json instanceof JSONArray) {
+            JSONArray arr = (JSONArray) json;
+            for (Object obj : arr) {
+                convertToUpperUnderline(obj);
+            }
+        } else if (json instanceof JSONObject) {
+            JSONObject jo = (JSONObject) json;
+            Set<String> keys = jo.keySet();
+            String[] array = keys.toArray(new String[keys.size()]);
+            for (String key : array) {
+            	Pattern pattern = Pattern.compile("^[A-Z0-9_]*$");
+            	Matcher matcher = pattern.matcher(key);
+                Object value = jo.get(key);
+              //如果key已经全部大写，就不再转化
+            	if(!matcher.matches()){
+            		String replace = key.replace("_", "");
+                    jo.remove(key);
+                    jo.put(underscoreName(replace), value);
+            	}
+                    convertToUpperUnderline(value);
+            }
+        }
+    }
+    
+    
+    /**
+     * 适用场景: 递归转换json key为驼峰
+     * 调用方式: 
+     * 业务逻辑说明
+    */
+    private final static void convertToCamel(Object json) {
+        if (json instanceof JSONArray) {
+            JSONArray arr = (JSONArray) json;
+            for (Object obj : arr) {
+                convertToCamel(obj);
+            }
+        } else if (json instanceof JSONObject) {
+            JSONObject jo = (JSONObject) json;
+            Set<String> keys = jo.keySet();
+            String[] array = keys.toArray(new String[keys.size()]);
+            for (String key : array) {
+                Object value = jo.get(key);
+                String camelCaseName = camelCaseName(key);
+                jo.remove(key);
+                jo.put(camelCaseName, value);
+                convertToCamel(value);
+            }
+        }
+    }
+ 
+    /**
+     * 适用场景: 转换String类型的json串，的 key为驼峰例如：STAFF_INFO_REQ转为staffInfoReq
+     * 调用方式: 
+     * 业务逻辑说明
+    */
+    public final static Object convertToCamel(String json) {
+        Object obj = JSON.parse(json);
+        convertToCamel(obj);
+        return obj;
+    }
+
+   /* public static void main(String[] args) {
+        String a="{\"operStaff\":\"SZXX0022\",\n" +
+                "\"provinceCode\":\"51\",\n" +
+                "\"eparchyCode\":\"0755\",\n" +
+                "\"cityCode\":\"0755\",\n" +
+                "\"channelId\":\"5110053\",\n" +
+                "\"channelType\":\"3000000\",\n" +
+                "\"operDepartCode\":\"5110053\",\n" +
+                "\"busiDataJson\":\"null\",\n" +
+                "\"current\":\"1\",\n" +
+                "\"pageSize\":\"10\",\n" +
+                "\"offset\":\"0\",\n" +
+                "\"limit\":\"10\",\n" +
+                "\"batchId\":\"null\",\n" +
+                "\"serialNumber\":\"null\",\n" +
+                "\"status\":\"1\",\n" +
+                "\"oldStatus\":\"null\",\n" +
+                "\"startTime\":\"2019-05-10 00:00:00\",\n" +
+                "\"endTime\":\"2019-05-10 23:59:59\",\n" +
+                "\"queryStaffNo\":\"\",\n" +
+                "\"batchType\":\"1\"}";
+        System.out.println(convertToUpperUnderline(a));
+    }*/
+
+}
---
+++ springboot-admin-client/src/main/java/com/ctg/test/utils/MoneyFormatUitl.java
@@ -0,0 +1,156 @@
+package com.ctg.test.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+public class MoneyFormatUitl {
+    /** 分转元<br>
+     * 适用场景: <br>
+     * 调用方式: <br>
+     * 业务逻辑说明<br>*/
+    public static String changeF2Y(String fenStr) {
+        String yuanStr = "";
+
+        if (StringUtils.isBlank(fenStr)) {
+            return "";
+        }
+
+        if (!fenStr.matches("\\-?[0-9]+")) {
+            return "0.00";
+        }
+
+        int flag = 0;
+        String amString = fenStr.trim();
+        if (amString.charAt(0) == '-') {
+            flag = 1;
+            amString = amString.substring(1);
+        }
+        StringBuffer result = new StringBuffer();
+        if (amString.length() == 1) {
+            result.append("0.0").append(amString);
+        }
+        else if (amString.length() == 2) {
+            result.append("0.").append(amString);
+        }
+        else {
+            String intString = amString.substring(0, amString.length() - 2);
+            for (int i = 1; i <= intString.length(); i++) {
+                // if ((i - 1) % 3 == 0 && i != 1) {
+                // result.append(",");
+                // }
+                result.append(intString.substring(intString.length() - i, intString.length() - i + 1));
+            }
+            result.reverse().append(".").append(amString.substring(amString.length() - 2));
+        }
+        if (flag == 1) {
+            yuanStr = "-" + result.toString();
+        }
+        else {
+            yuanStr = result.toString();
+        }
+
+        return yuanStr;
+
+    }
+
+    /** 分转元，带千分位表示 <br>
+     * 适用场景: <br>
+     * 调用方式: <br>
+     * 业务逻辑说明<br>*/
+    public static String changeF2YT(String fenStr) {
+        String yuanStr = "";
+
+        if (!fenStr.matches("\\-?[0-9]+")) {
+            return "0.00";
+        }
+
+        int flag = 0;
+        String amString = fenStr.trim();
+        if (amString.charAt(0) == '-') {
+            flag = 1;
+            amString = amString.substring(1);
+        }
+        StringBuffer result = new StringBuffer();
+        if (amString.length() == 1) {
+            result.append("0.0").append(amString);
+        }
+        else if (amString.length() == 2) {
+            result.append("0.").append(amString);
+        }
+        else {
+            String intString = amString.substring(0, amString.length() - 2);
+            for (int i = 1; i <= intString.length(); i++) {
+                if ((i - 1) % 3 == 0 && i != 1) {
+                    result.append(",");
+                }
+                result.append(intString.substring(intString.length() - i, intString.length() - i + 1));
+            }
+            result.reverse().append(".").append(amString.substring(amString.length() - 2));
+        }
+        if (flag == 1) {
+            yuanStr = "-" + result.toString();
+        }
+        else {
+            yuanStr = result.toString();
+        }
+
+        return yuanStr;
+
+    }
+
+    /** 元转分 <br>
+     * 适用场景: <br>
+     * 调用方式: <br>
+     * 业务逻辑说明<br>*/
+    public static Long parseYuanToFen(String yunaStr) {
+        if (StringUtils.isBlank(yunaStr)) {
+            return 0L;
+        }
+        else {
+            String currency = yunaStr.replaceAll("\\$|\\￥|\\,", ""); // 处理包含, ￥ 或者$的金额
+            int index = currency.indexOf(".");
+            int length = currency.length();
+            Long amLong = 0l;
+            if (index == -1) {
+                amLong = Long.valueOf(currency + "00");
+            }
+            else if (length - index >= 3) {
+                amLong = Long.valueOf((currency.substring(0, index + 3)).replace(".", ""));
+            }
+            else if (length - index == 2) {
+                amLong = Long.valueOf((currency.substring(0, index + 2)).replace(".", "") + 0);
+            }
+            else {
+                amLong = Long.valueOf((currency.substring(0, index + 1)).replace(".", "") + "00");
+            }
+            return amLong;
+        }
+    }
+
+    /**
+     * @description:厘转分
+     */
+    public static String li2fen(String li){
+        if(StringUtils.isBlank(li)||"0".equalsIgnoreCase(li)||li.length()<=1){
+            return "0";
+        }
+        else {
+            String fen = li.substring(0, li.length() - 1);
+            return fen;
+        }
+    }
+    /**
+     * @description:分转厘
+     */
+    public static String fen2li(String fen){
+        if(StringUtils.isBlank(fen)||"0".equalsIgnoreCase(fen)){
+            return "0";
+        }
+        else {
+            String li = fen+"0";
+            return li;
+        }
+    }
+
+
+}
